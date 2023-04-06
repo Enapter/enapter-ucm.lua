4fcbaebc1204cd1988aad63c7bfa77c408b901c2
Aleksey Bakin <alexey.bakin@gmail.com>
2023-04-06 16:19:41 +0300

    chore: add tools to generate new release

---
+++ tools/new_release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+version=$1
+
+if [ -z "$version" ]; then
+    echo "Usage: ./tools/new_release.sh 0.2.1"
+    exit 1
+fi
+
+scm_name="enapter-ucm-scm-1.rockspec"
+release_name=${scm_name//scm/$version}
+cp "$scm_name" "rockspecs/$release_name"
+
+sed -i '' "s/rock_version = 'scm'/rock_version = '$version'/" "rockspecs/$release_name"
+
+prev_release=$(git tag|tail -1)
+date=$(date '+%B %-d, %Y')
+changelog=$(git log --pretty=format:%s "$prev_release"..HEAD | sed 's/^/\* /g')
+echo -e "## $version ($date)\n$changelog\n" | cat - CHANGELOG.md > CHANGELOG.temp && mv CHANGELOG.temp CHANGELOG.md
